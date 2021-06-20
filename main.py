0fe34f51c252337a60c8469dcfc3c9190bd0e87b
mari44ka <marikostenko83@gmail.com>
2021-06-19 23:42:46 -0700

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import requests
 response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
+
+response2 = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response2.text)
